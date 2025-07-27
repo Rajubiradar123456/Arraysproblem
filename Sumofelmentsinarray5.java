a64185064845be883c98681972ce3a0102eff170
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-07-27 23:22:36 +0530

    sum of the elements in  the array

---
+++ Sumofelmentsinarray5.java
@@ -0,0 +1,10 @@
+public class Sumofelmentsinarray5 {
+    public static void main(String[] args) {
+        int arr[]={2,4,52,4,6,32,5};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
