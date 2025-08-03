fa2eb921359b9f1fe043087fb67001dca6492bd9
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-08-03 12:25:58 +0530

    Minimum Scalar product of two array

---
+++ MinimumscalrProductoftwovectors14.java
@@ -0,0 +1,36 @@
+
+// one ascending and another one desending
+public class MinimumscalrProductoftwovectors14 {
+    public static void main(String[] args) {
+        int arr1[]={1, 2, 6, 3, 7};
+        int arr2[] = {10, 7, 45, 3, 7};
+        int n=arr1.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr1[i]>arr1[j]){
+                    int temp=arr1[i];
+                    arr1[i]=arr1[j];
+                    arr1[j]=temp;
+                }
+            }
+        }
+
+         for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr2[i]<arr2[j]){
+                    int temp=arr2[i];
+                    arr2[i]=arr2[j];
+                    arr2[j]=temp;
+                }
+            } 
+        }
+        int product=0;
+        for(int i=0;i<n;i++){
+            product += arr1[i]*arr2[i];
+        }
+        System.out.print(product);
+
+
+    }
+}
